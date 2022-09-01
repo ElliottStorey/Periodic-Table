305858a65020013f049c867839153d9224afb954
Elliott Storey <elliott.storey@outlook.com>
2022-09-01 10:12:15 -0500

    Create App

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <div>
+        <input placeholder='Enter Username'>Username</input>
+        <input placeholder='Enter password'>Password</input>
+        <button>Login</button>
+        <a>Sign Up</a>
+    </div>
+  );
+}
---
+++ src/Signup.js
@@ -0,0 +1,10 @@
+export default function Signup() {
+  return (
+    <div>
+        <input placeholder='Enter Username'>Username</input>
+        <input placeholder='Enter password'>Password</input>
+        <button>Sign Up</button>
+        <a>Login</a>
+    </div>
+  );
+}
